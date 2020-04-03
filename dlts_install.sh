ce4d387f22ffca1fb1111d006cf0a8a7bff4474f
zhuwe <zhuwe@microsoft.com>
2020-04-03 16:28:50 +0800

    add dlts running shell

---
+++ dlts_install.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+
+
+EXP_ID=$1
+solaris_branch=master
+
+#output dir setting
+WDATA_DIR=/data/zhuoyu/spacenet6/wdata
+OUTPUT_DIR=$WDATA_DIR/$EXP_ID
+mkdir $OUTPUT_DIR
+
+#envirement
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends bc bzip2 ca-certificates curl emacs git less libgdal-dev libssl-dev libffi-dev libncurses-dev libgl1 jq nfs-common parallel python-dev python-pip python-wheel python-setuptools tree unzip zip vim wget xterm build-essential
+
+cd /
+EXP_ROOT_DIR=/zhuoyu_exp
+sudo mkdir $EXP_ROOT_DIR
+sudo chmod 777 $EXP_ROOT_DIR
+cd $EXP_ROOT_DIR
+pwd
+ls
+
+
+
+#code
+CODE_DIR=${EXP_ROOT_DIR}/code
+mkdir ${CODE_DIR}
+cd ${CODE_DIR}
+git clone https://github.com/cosmiq/solaris.git
+cd solaris
+git checkout ${solaris_branch}
+sudo pip install git+git://github.com/toblerity/shapely.git
+cd solaris
+sudo pip install .
+
+
+
+
+traindatapath=/data/zhuoyu/spacenet6/unzip_dir/AOI_11_Rotterdam
+testdatapath=/data/zhuoyu/spacenet6/unzip_dir/test/test_public/AOI_11_Rotterdam
+
+traindataargs="\
+--sardir $traindatapath/SAR-Intensity \
+--opticaldir $traindatapath/PS-RGB \
+--labeldir $traindatapath/geojson_buildings \
+--rotationfile $traindatapath/SummaryData/SAR_orientations.txt \
+"
+
+dstdir=/root
+
+settings="\
+--rotationfilelocal $dstdir/SAR_orientations.txt \
+--maskdir $dstdir/masks \
+--sarprocdir $dstdir/sartrain \
+--opticalprocdir $dstdir/optical \
+--traincsv $dstdir/train.csv \
+--validcsv $dstdir/valid.csv \
+--opticaltraincsv $dstdir/opticaltrain.csv \
+--opticalvalidcsv $dstdir/opticalvalid.csv \
+--testcsv $dstdir/test.csv \
+--yamlpath $dstdir/sar.yaml \
+--opticalyamlpath $dstdir/optical.yaml \
+--modeldir $dstdir/weights \
+--testprocdir $dstdir/sartest \
+--testoutdir $dstdir/inference_continuous \
+--testbinarydir $dstdir/inference_binary \
+--testvectordir $dstdir/inference_vectors \
+--rotate \
+--transferoptical \
+--mintrainsize 20 \
+--mintestsize 80 \
+"
+
+
+python baseline.py --pretrain --train $traindataargs $settings
+
+
+
+outputpath=$OUTPUT_DIR
+testdataargs="\
+--testdir $testdatapath/SAR-Intensity \
+--outputcsv $outputpath \
+"
+
+
+python baseline.py --pretest --test $testdataargs $settings
